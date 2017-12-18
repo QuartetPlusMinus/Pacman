5e03347ce5e72ff857e949f29b86bf5f120b6c56
viewsharp <viewsharp@yandex.ru>
2017-12-18 23:12:55 +0300

    remove and rebuild some files

--- CMakeFiles/pacman_autogen.dir/AutogenOldSettings.cmake
+++
@@ -1,2 +0,0 @@
-set(AM_MOC_OLD_SETTINGS "QT_CORE_LIB;QT_GUI_LIB;QT_WIDGETS_LIB ~~~ /home/murt/GitHub/Pacman/pacman_autogen/include;/home/murt/GitHub/Pacman;/usr/local/include;/home/murt/GitHub/Pacman/generated;/usr/local;/home/murt/Qt/5.10.0/gcc_64/include;/home/murt/Qt/5.10.0/gcc_64/include/QtWidgets;/home/murt/Qt/5.10.0/gcc_64/include/QtGui;/home/murt/Qt/5.10.0/gcc_64/include/QtCore;/home/murt/Qt/5.10.0/gcc_64/./mkspecs/linux-g++;/usr/include ~~~  ~~~ FALSE ~~~ ")
-set(AM_UIC_OLD_SETTINGS " ~~~  ~~~ ")
--- CMakeFiles/pacman_server_autogen.dir/AutogenOldSettings.cmake
+++
@@ -1,2 +0,0 @@
-set(AM_MOC_OLD_SETTINGS " ~~~ /home/murt/GitHub/Pacman/pacman_server_autogen/include;/home/murt/GitHub/Pacman;/usr/local/include;/home/murt/GitHub/Pacman/generated;/usr/local ~~~  ~~~ FALSE ~~~ ")
-set(AM_UIC_OLD_SETTINGS " ~~~  ~~~ ")
